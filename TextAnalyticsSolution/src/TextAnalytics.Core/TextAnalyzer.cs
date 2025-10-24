c2f751eaae2dfeeac053759c8456ddb7a3051ea8
szymon <szymon.pt4sznik@gmail.com>
2025-10-24 17:36:12 +0200

    implemented simpler counting characters methods

--- TextAnalyticsSolution/src/TextAnalytics.Core/TextAnalyzer.cs
+++ TextAnalyticsSolution/src/TextAnalytics.Core/TextAnalyzer.cs
@@ -2,23 +2,97 @@ namespace TextAnalytics.Core;
 
 public interface ITextAnalyzer
 {
-    public int CountCharacters(string text);
-    public int CountCharactersNoSpaces(string text);
-    public int CountLetters(string text);
-    public int CountDigits(string text);
-    public int CountPunctuation(string text);
-    public int CountWords(string text);
-    public int CountUniqueWords(string text);
-    public string MostCommonWord(string text);
-    public float AvgWordLength(string text);
-    public string LongestWord(string text);
-    public string ShortestWord(string text);
-    public int CountSentences(string text);
-    public float AvgWordsPerSentence(string text);
-    public string LongestSentence(string text);
+    public AnalysisRecord Analyze(string text);
+    protected int CountCharacters(string text);
+    protected int CountCharactersNoSpaces(string text);
+    protected int CountLetters(string text);
+    protected int CountDigits(string text);
+    protected int CountPunctuation(string text);
+    protected int CountWords(string text);
+    protected int CountUniqueWords(string text);
+    protected string MostCommonWord(string text);
+    protected float AvgWordLength(string text);
+    protected string LongestWord(string text);
+    protected string ShortestWord(string text);
+    protected int CountSentences(string text);
+    protected float AvgWordsPerSentence(string text);
+    protected string LongestSentence(string text);
 }
 
-public class TextAnalyzer
+public class TextAnalyzer : ITextAnalyzer
 {
-    
+    public AnalysisRecord Analyze(string text)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int CountCharacters(string text)
+    {
+        return text.Length;
+    }
+
+    public int CountCharactersNoSpaces(string text)
+    {
+        return text.Replace(" ", "").Length;
+    }
+
+    public int CountLetters(string text)
+    {
+        return text.Count(char.IsLetter);
+    }
+
+    public int CountDigits(string text)
+    {
+        return text.Count(char.IsDigit);
+    }
+
+    public int CountPunctuation(string text)
+    {
+        return text.Count(char.IsPunctuation);
+    }
+
+    public int CountWords(string text)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int CountUniqueWords(string text)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string MostCommonWord(string text)
+    {
+        throw new NotImplementedException();
+    }
+
+    public float AvgWordLength(string text)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string LongestWord(string text)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ShortestWord(string text)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int CountSentences(string text)
+    {
+        throw new NotImplementedException();
+    }
+
+    public float AvgWordsPerSentence(string text)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string LongestSentence(string text)
+    {
+        throw new NotImplementedException();
+    }
 }
