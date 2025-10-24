2414dbd319fdd07f3ff68554e897c048cf48bd51
szymon <szymon.pt4sznik@gmail.com>
2025-10-24 13:49:22 +0200

    main program recognize different command line arguments

--- TextAnalyticsSolution/src/TextAnalytics.App/Program.cs
+++ TextAnalyticsSolution/src/TextAnalytics.App/Program.cs
@@ -1,3 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace TextAnalytics.App 
+{
+    internal class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            string? filePath = null;
+            
+            if (args.Length > 0)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] == "--file" || args[i] == "-f")
+                    {
+                        if (i + 1 < args.Length)
+                        {
+                            filePath = args[i + 1];
+                        }
+                        else
+                        {
+                            Console.WriteLine("No file specified");
+                            Environment.Exit(1);
+                        }
+
+                        break;
+                    }
+
+                    if (args[i] == "--interactive" || args[i] == "-i")
+                    {
+                        break;
+                    }
+
+                }
+            }
+
+            if (filePath == null)
+            {
+                Console.WriteLine("Text from stdin");
+            }
+            else
+            {
+                Console.WriteLine($"Text from file: {filePath}");
+            }
+
+        }
+    }
+}
