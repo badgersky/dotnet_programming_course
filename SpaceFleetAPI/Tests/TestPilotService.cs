7c626bf3af771743471fb7fd9f89161046d7ca44
szymon <szymon.pt4sznik@gmail.com>
2025-11-22 01:52:19 +0100

    tests for pilot service

--- SpaceFleetAPI/Tests/TestPilotService.cs
+++ SpaceFleetAPI/Tests/TestPilotService.cs
@@ -40,4 +40,56 @@ public class PilotServiceTests
         Assert.That(pilots.Count, Is.EqualTo(1));
         Assert.That(result, Is.True);
     }
+    
+    [Test]
+    public async Task TestDeletePilot()
+    {
+        var pilot = new Pilot { Name = "Jerry" };
+        var result1 = await _s.Create(pilot);
+        var pilots1 = await _s.ReadAll();
+
+        var result2 = await _s.Delete(pilot.Id);
+        var pilots2 = await _s.ReadAll();
+        
+        Assert.That(pilots1, Is.Not.Null);
+        Assert.That(pilots1.Count, Is.EqualTo(1));
+        Assert.That(result1, Is.True);
+        Assert.That(result2, Is.True);
+        Assert.That(pilots2, Is.Not.Null);
+        Assert.That(pilots2.Count, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public async Task TestReadOnePilot()
+    {
+        var pilot = new Pilot { Name = "Jerry" };
+        var result1 = await _s.Create(pilot);
+        
+        pilot = new Pilot { Name = "Jane" };
+        var result2 =  await _s.Create(pilot);
+        
+        var pilot2 = await _s.ReadOne(pilot.Id);
+        
+        Assert.That(result1, Is.True);
+        Assert.That(result2, Is.True);
+        Assert.That(pilot2, Is.Not.Null);
+        Assert.That(pilot2?.Name, Is.EqualTo("Jane"));
+    }
+    
+    [Test]
+    public async Task TestUpdatePilot()
+    {
+        var pilot = new Pilot { Name = "Jerry" };
+        var result1 = await _s.Create(pilot);
+        
+        var uPilot = new Pilot { Name = "Jane" };
+        var result2 =  await _s.Update(pilot.Id, uPilot);
+        
+        var pilot2 = await _s.ReadOne(pilot.Id);
+        
+        Assert.That(result1, Is.True);
+        Assert.That(result2, Is.True);
+        Assert.That(pilot2, Is.Not.Null);
+        Assert.That(pilot2?.Name, Is.EqualTo("Jane"));
+    }
 }
